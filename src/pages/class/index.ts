3ec700cad8d46320f64411a36390092295e0461c
Peng.Jin <peng.jin@symbio.com>
2021-11-12 10:18:59 +0800

    ok

--- src/pages/class/index.ts
+++ src/pages/class/index.ts
@@ -1,11 +1,33 @@
-import { View, Text } from '@tarojs/components'
+import React, { useEffect } from 'react'
+import { View } from '@tarojs/components'
+import {
+  useReady,
+  useDidShow,
+  useDidHide,
+  usePullDownRefresh
+} from '@tarojs/taro'
 
-export default () => {
+function Class () {
+  // 可以使用所有的 React Hooks
+  useEffect(() => {})
+
+  // 对应 onReady
+  useReady(() => {})
+
+  // 对应 onShow
+  useDidShow(() => {})
+
+  // 对应 onHide
+  useDidHide(() => {})
+
+  // Taro 对所有小程序页面生命周期都实现了对应的自定义 React Hooks 进行支持
+  // 详情可查阅：【Hooks】
+  usePullDownRefresh(() => {})
+
   return (
-    <View>
-      <Text>
-        Class
-      </Text>
-    </View>
+    <View className='index' />
   )
 }
+
+export default Class
+
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,11 +1,32 @@
-import { View, Text } from '@tarojs/components'
+import React, { useEffect } from 'react'
+import { View } from '@tarojs/components'
+import {
+  useReady,
+  useDidShow,
+  useDidHide,
+  usePullDownRefresh
+} from '@tarojs/taro'
 
-export default () => {
+function Index () {
+  // 可以使用所有的 React Hooks
+  useEffect(() => {})
+
+  // 对应 onReady
+  useReady(() => {})
+
+  // 对应 onShow
+  useDidShow(() => {})
+
+  // 对应 onHide
+  useDidHide(() => {})
+
+  // Taro 对所有小程序页面生命周期都实现了对应的自定义 React Hooks 进行支持
+  // 详情可查阅：【Hooks】
+  usePullDownRefresh(() => {})
+
   return (
-    <View>
-      <Text>
-        Index
-      </Text>
-    </View>
+    <View className='index' />
   )
 }
+
+export default Index
